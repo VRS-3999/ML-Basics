1fa01790442d8986a8c75026424969a3334291b7
VRS-3999 <vivekrajs3999@gmail.com>
2020-01-29 08:39:45 -0800

    Create Histogram

---
+++ Histogram.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+def load_housing_data(housing_path = HOUSING_PATH):
+    csv_path = os.path.join(housing_path,"housing.csv")
+    return pd.read_csv(csv_path)
+
+if __name__ == 'main':
+#    %matplotlib inline # only in a Jupyter notebook
+    housing = load_housing_data()
+    housing.hist(bins=50, figsize=(20,15))
+    plt.show()
