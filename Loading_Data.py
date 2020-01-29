3694f5edf5ef773e7878af3b30f1f15b821df725
VRS-3999 <vivekrajs3999@gmail.com>
2020-01-29 09:03:21 -0800

    Loading Csv Data

--- Loading_Data.py
+++ Loading_Data.py
@@ -7,8 +7,9 @@ def load_housing_data(housing_path = HOUSING_PATH):
     csv_path = os.path.join(housing_path,"housing.csv")
     return pd.read_csv(csv_path)
 
-housing = load_housing_data()
-housing.head()    
-housing.info()
-housing["ocean_proximity"].value_counts()    
-housing.describe()
+if __name__ == '__main__':
+    housing = load_housing_data()
+    housing.head()    
+    housing.info()
+    housing["ocean_proximity"].value_counts()    
+    housing.describe()
