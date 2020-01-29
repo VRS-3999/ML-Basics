fa4ae04729a6cd646f556303ddd0433ec8ccfea5
VRS-3999 <vivekrajs3999@gmail.com>
2020-01-29 11:35:44 -0800

    Stratified Sampling

--- Stratified_Sampling.py
+++ Stratified_Sampling.py
@@ -1,12 +1,16 @@
 from sklearn.model_selection import StratifiedShuffleSplit
 
-housing["income_cat"] = np.ceil(housing["median_income"]/1.5)
-housing["income_cat"].where(housing["income_cat"]<5, 5.0, inplace=True)
-
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index]
-    strat_test_set = housing.loc[test_index]
-
 if __name__ == '__main__':
+    
+    housing["income_cat"] = np.ceil(housing["median_income"]/1.5)
+    housing["income_cat"].where(housing["income_cat"]<5, 5.0, inplace=True)
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
     print(housing["income_cat"].value_counts() / len(housing))
+
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
