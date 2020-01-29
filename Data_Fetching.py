1e2c1811f5bc5aa7b0482634f8eeb0e9a7083d97
VRS-3999 <vivekrajs3999@gmail.com>
2020-01-29 08:00:58 -0800

    Data Fetching

--- Data_Fetching.py
+++ Data_Fetching.py
@@ -9,8 +9,11 @@ HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
         os.makedirs(housing_path)
-    tgz_path = os.path.join(housing_path,"housing.tgz")
+    tgz_path = os.path.join(housing_path, "housing.tgz")
     urllib.request.urlretrieve(housing_url, tgz_path)
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+if __name__ == '__main__':
+    fetch_housing_data();
