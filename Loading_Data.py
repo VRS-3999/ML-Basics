bb1a537803af8508c3f68bda8f341d00ae3df431
VRS-3999 <vivekrajs3999@gmail.com>
2020-01-29 07:46:26 -0800

    Loading Data

---
+++ Loading_Data.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+def load_housing_data(housing_path = HOUSING_PATH):
+    csv_path = os.path.join(housing_path,"housing.csv")
+    return pd.read_csv(csv_path)
+
+if __name__ == '__main__':
+    housing = load_housing_data()
+#   housing.head()     #Not working
+    housing.info()
