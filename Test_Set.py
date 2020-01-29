059f5e444ecfd7595d332cc6ca57cd674771fcee
VRS-3999 <vivekrajs3999@gmail.com>
2020-01-29 09:40:32 -0800

    Create Test Set

--- Test_Set.py
+++ Test_Set.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def split_train_test(data, test_ratio):
-    # np.random.seed(data)
+    # np.random.seed(42)
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data)*test_ratio)
     test_indices = shuffled_indices[:test_set_size]
