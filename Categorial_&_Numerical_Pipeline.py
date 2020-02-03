c4e803d0881670027803138c98546f6984d8d12b
VRS-3999 <vivekrajs3999@gmail.com>
2020-02-03 10:29:35 -0800

    creating different pipelines for categorial and numerical data

--- Categorial_&_Numerical_Pipeline.py
+++ Categorial_&_Numerical_Pipeline.py
@@ -2,6 +2,14 @@ if __name__ == '__main__':
     num_attribs = list(housing_num)
     cat_attribs = ["ocean_proximity"]
 
+    class NewLabelBinarizer(LabelBinarizer):
+        def fit(self, X, y=None):
+            return super(NewLabelBinarizer, self).fit(X)
+        def transform(self, X, y=None):
+            return super(NewLabelBinarizer, self).transform(X)
+        def fit_transform(self, X, y=None):
+            return super(NewLabelBinarizer, self).fit(X).transform(X)
+    
     num_pipeline = Pipeline([
         ('selector', DataFrameSelector(num_attribs)),
         ('imputer', Imputer(strategy = "median")),
@@ -10,5 +18,5 @@ if __name__ == '__main__':
         ])
     cat_pipeline = Pipeline([
         ('selector', DataFrameSelector(cat_attribs)),
-        ('label_binarizer', LabelBinarizer()),
+        ('label_binarizer', NewLabelBinarizer()),
         ])
